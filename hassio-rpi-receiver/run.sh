2ec2ae23e960f74b5641fb48300f0651d9cc0d7c
avlisiur78 <80415671+avlisiur78@users.noreply.github.com>
2021-03-25 23:39:55 +0000

    Update run.sh

--- hassio-rpi-receiver/run.sh
+++ hassio-rpi-receiver/run.sh
@@ -4,7 +4,7 @@ set -e
 requirements=$(cat /data/options.json | jq -r 'if .requirements then .requirements | join(" ") else "" end')
 code=$(cat /data/options.json | jq -r '.code')
 clean=$(cat /data/options.json | jq -r '.clean //empty')
-py3=$(cat /data/options.json | jq -r '.python2 // empty')
+py3=$(cat /data/options.json | jq -r '.python3 // empty')
 
 PYTHON=$(which python3)
 
