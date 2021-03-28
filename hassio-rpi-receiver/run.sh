ebf7a34626357a30d54464694c079af85e6a5409
avlisiur78 <80415671+avlisiur78@users.noreply.github.com>
2021-03-28 21:01:30 +0100

    Update run.sh

--- hassio-rpi-receiver/run.sh
+++ hassio-rpi-receiver/run.sh
@@ -4,14 +4,8 @@ set -e
 requirements=$(cat /data/options.json | jq -r 'if .requirements then .requirements | join(" ") else "" end')
 code=$(cat /data/options.json | jq -r '.code')
 clean=$(cat /data/options.json | jq -r '.clean //empty')
-py3=$(cat /data/options.json | jq -r '.python3 // empty')
 
 PYTHON=$(which python3)
-
-if [ "${py3}" == "true" ];
-then
-    PYTHON=$(which python3)
-fi
 
 if [ -n "$requirements" ];
 then
@@ -28,4 +22,4 @@ then
     fi
     pip install -U ${requirements}
 fi
-python3 ${code} 
+python ${code} 
