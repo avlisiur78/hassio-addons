2cd74ed725740c03557d8f10c271fa90e8100f30
avlisiur78 <80415671+avlisiur78@users.noreply.github.com>
2021-04-01 10:00:01 +0100

    Update run.sh

--- hassio-rpi-rf/run.sh
+++ hassio-rpi-rf/run.sh
@@ -4,13 +4,13 @@ set -e
 requirements=$(cat /data/options.json | jq -r 'if .requirements then .requirements | join(" ") else "" end')
 code=$(cat /data/options.json | jq -r '.code')
 clean=$(cat /data/options.json | jq -r '.clean //empty')
-py2=$(cat /data/options.json | jq -r '.python2 // empty')
+py3=$(cat /data/options.json | jq -r '.python3 // empty')
 
 PYTHON=$(which python3)
 
-if [ "${py2}" == "true" ];
+if [ "${py3}" == "true" ];
 then
-    PYTHON=$(which python2)
+    PYTHON=$(which python3)
 fi
 
 if [ -n "$requirements" ];
