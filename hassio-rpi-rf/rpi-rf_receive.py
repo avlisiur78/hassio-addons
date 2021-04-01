c31010d493d60e98271de6f3f5393d2253277338
avlisiur78 <80415671+avlisiur78@users.noreply.github.com>
2021-04-01 10:57:59 +0100

    Update rpi-rf_receive.py

--- hassio-rpi-rf/rpi-rf_receive.py
+++ hassio-rpi-rf/rpi-rf_receive.py
@@ -7,7 +7,7 @@ import time
 import logging
 import io, os, datetime, shutil
 
-mosquitto_address = "192.168.1.100"
+mosquitto_address = "core-mosquitto"
 mosquitto_port = "1883"
 mosquitto_user = "homeassistant"
 mosquitto_password = "REDACTED" 
