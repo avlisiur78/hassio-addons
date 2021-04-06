b4de8bf8b5dc8c5596e396266aef0e5e6fdfd628
avlisiur78 <80415671+avlisiur78@users.noreply.github.com>
2021-04-06 18:53:29 +0100

    Update run.sh

--- hassio-rpi-rf/run.sh
+++ hassio-rpi-rf/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-set -e
+#set -e
 
-requirements=$(cat /data/options.json | jq -r 'if .requirements then .requirements | join(" ") else "" end')
-code=$(cat /data/options.json | jq -r '.code')
-clean=$(cat /data/options.json | jq -r '.clean //empty')
-py3=$(cat /data/options.json | jq -r '.python3 // empty')
+#requirements=$(cat /data/options.json | jq -r 'if .requirements then .requirements | join(" ") else "" end')
+#code=$(cat /data/options.json | jq -r '.code')
+#clean=$(cat /data/options.json | jq -r '.clean //empty')
+#py3=$(cat /data/options.json | jq -r '.python3 // empty')
 
-PYTHON=$(which python3)
+#PYTHON=$(which python3)
 
-if [ "${py3}" == "true" ];
-then
-    PYTHON=$(which python3)
-fi
+#if [ "${py3}" == "true" ];
+#then
+#    PYTHON=$(which python3)
+#fi
 
 python3 ${code} 
