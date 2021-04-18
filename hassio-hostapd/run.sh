52e6a01723714c7b119f2b37949fc4536eaee710
avlisiur78 <80415671+avlisiur78@users.noreply.github.com>
2021-04-19 00:05:06 +0100

    Update run.sh

--- hassio-hostapd/run.sh
+++ hassio-hostapd/run.sh
@@ -32,6 +32,9 @@ NETMASK=$(jq --raw-output ".netmask" $CONFIG_PATH)
 BROADCAST=$(jq --raw-output ".broadcast" $CONFIG_PATH)
 INTERFACE=$(jq --raw-output ".interface" $CONFIG_PATH)
 ALLOW_INTERNET=$(jq --raw-output ".allow_internet" $CONFIG_PATH)
+BLOCK_INTRANET=$(jq --raw-output ".block_intranet" $CONFIG_PATH)
+INTRANET_IP_RANGE=$(jq --raw-output ".intranet_ip_range" $CONFIG_PATH)
+INTRANET_IPS_EXCLUDE=$(jq --raw-output ".intranet_ips_exclude" $CONFIG_PATH)
 
 DHCP_SERVER=$(jq --raw-output ".dhcp_enable" $CONFIG_PATH)
 DHCP_START=$(jq --raw-output ".dhcp_start" $CONFIG_PATH)
@@ -87,17 +90,37 @@ INTERNET_IF="eth0"
 RULE_3="POSTROUTING -o ${INTERNET_IF} -j MASQUERADE"
 RULE_4="FORWARD -i ${INTERNET_IF} -o ${INTERFACE} -m state --state RELATED,ESTABLISHED -j ACCEPT"
 RULE_5="FORWARD -i ${INTERFACE} -o ${INTERNET_IF} -j ACCEPT"
+RULE_6="INPUT -s ${INTRANET_IP_RANGE} -j DROP"
 
 echo "Deleting iptables"
 iptables -v -t nat -D $(echo ${RULE_3})
 iptables -v -D $(echo ${RULE_4})
 iptables -v -D $(echo ${RULE_5})
+echo "Deleting iptables IPs Excluded"
+IPS=$(echo $INTRANET_IPS_EXCLUDE | tr "," "\n")
+for IP in $IPS
+do
+iptables -v -D INPUT -s $(echo ${IP} -j ACCEPT) 
+done
+echo "Deleting IP Range"
+iptables -v -D $(echo ${RULE_6})
 
 if test ${ALLOW_INTERNET} = true; then
     echo "Configuring iptables for NAT"
     iptables -v -t nat -A $(echo ${RULE_3})
     iptables -v -A $(echo ${RULE_4})
     iptables -v -A $(echo ${RULE_5})
+fi
+
+# Block intranet
+if test ${BLOCK_INTRANET} = true; then
+    echo "Blocking intranet"
+    IPS=$(echo $INTRANET_IPS_EXCLUDE | tr "," "\n")
+    for IP in $IPS
+    do
+    iptables -v -A INPUT -s $(echo ${IP} -j ACCEPT) 
+    done
+    iptables -v -A $(echo ${RULE_6})
 fi
 
 
